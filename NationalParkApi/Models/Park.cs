fb35030f76081de3108696bd1cea67e8c803f4d6
pphila <efl2ice89@gmail.com>
2023-02-11 12:57:49 -0800

    add migrations for required fields

--- NationalParkApi/Models/Park.cs
+++ NationalParkApi/Models/Park.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NationalParkApi.Models
 {
   public class Park
   {
     public int ParkId { get; set; }
+
+    [Required]
+    [StringLength(25)]
     public string Name { get; set; }
+    [Required]
+    [StringLength(20)]
     public string State { get; set; }
+    [Required]
     public string Description { get; set; }
+    [Required]
+    [StringLength(20)]
     public string DateEst { get; set; }
   }
 }
