23b077b9474aadf6f4c9c32ba6fff1c0428595b2
pphila <efl2ice89@gmail.com>
2023-02-11 12:23:45 -0800

    add delete funtionality to endpoint

--- NationalParkApi/Controllers/ParksController.cs
+++ NationalParkApi/Controllers/ParksController.cs
@@ -79,6 +79,20 @@ namespace NationalParkApi.Controllers
       return _db.Parks.Any(e => e.ParkId == id);
     }
 
-    
+    // DELETE: api/parks/7
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeletePark(int id)
+    {
+      Park park = await _db.Parks.FindAsync(id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+
+      _db.Parks.Remove(park);
+      await _db.SaveChangesAsync();
+
+      return NoContent();
+    }
   }
 }
